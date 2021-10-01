8052672db58cf4d9ac56d53fbf3f3768bcd6ce55
Jany Santana <76975431+janysantana@users.noreply.github.com>
2021-10-01 18:58:08 -0400

    added JS to modal

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-$(function () {
-  $("a[class='Update']").click(function () {
-    $("#MyPopup").modal("show");
-    return false;
-  });
-});
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
