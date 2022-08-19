4eeb07c19e47a85461de19abf1c7d40b5a7d4b6a
Jany Santana <76975431+janysantana@users.noreply.github.com>
2022-08-19 07:48:48 -0400

    changes to nav

--- mail.php
+++ mail.php
@@ -1,6 +1,10 @@
 <?php 
 
 if (isset($_POST['submit'])) {
+
+header("Location: index.php?mailsend");
+}
+
 $name = $_POST['name'];
 $mailFrom = $_POST['email'];
 $message = $_POST['message'];
@@ -8,8 +12,7 @@ $message = $_POST['message'];
 $formcontent="From: ".$name.".\n\n".$message;
 $mailTo = "contact@janysantana.com";
 $header = "From: ".$mailFrom;
-
+echo "Test";
 mail($mailTo, $formcontent, $header);
-header("Location: index.php?mailsend");
-}
+echo "Test";
 ?>
